03e4f48156a892e3fdd5cbde4c20826319cb2a50
unknown <noah.rissatii@gmail.com>
2023-09-20 17:02:21 -0300

    feat: implementing main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite as 7 temperaturas separadas por espaço: ");
+        System.out.print("Digite as temperaturas separadas por espaço: ");
+        String temperaturasStr = scanner.nextLine();
 
+        double[] temperaturas = GuardarTemperatura.lerTemperaturas(temperaturasStr);
 
+        double media = CalcularMedia.calcularMedia(temperaturas);
+        double temperaturasAcima = CalcularAcimaDaMedia.calcularAcimaDaMedia(temperaturas,media);
+        System.out.println(temperaturasAcima);
+
+        scanner.close();
     }}
